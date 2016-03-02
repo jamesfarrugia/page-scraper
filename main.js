1017cf44df705d138523aa7fa0bd45b74dc1d686
James Farrugia <jamsinux@gmail.com>
2016-03-02 19:51:46 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,43 @@
+var page = require('webpage').create();
+var url  = "http://exiplatform.com/sample.html";
+var scraper = {};
+
+page.onError = function (msg, trace)
+{
+	console.trace(msg);
+	phantom.exit();
+};
+
+page.onResourceError = function(resourceError)
+{
+	console.error(resourceError.url + ': ' + resourceError.errorString);
+};
+
+page.onAlert = function(msg)
+{
+	console.log("[alert] - " + msg);
+};
+
+scraper.onOpen = function()
+{
+	console.log("jquery injected");
+
+	var evaluated = page.evaluate(function()
+	{
+		console.log("page evaulated");
+		return $("html").html();
+	});
+
+	console.log("------------evaluation result-----------------");
+	console.log(evaluated);
+
+	phantom.exit();
+}
+
+page.open(url, function(status)
+{
+	console.log("open - " + url +  " - "  + status);
+
+	page.includeJs('https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js',
+	scraper.onOpen);
+});
